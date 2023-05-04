29f324271ede9757c6a30b1e503d0a1cd7cd7465
Alana Barrett-Frew <alanabarrett-frew@hotmail.com>
2023-05-04 20:30:24 +0100

    bootstrap example added and edited

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
 const express = require("express");
-const https = require("https");
+const request = require("request");
 const bodyParser = require("body-parser");
 
 const app = express();
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/signup.html");
-});
-
 
 
 app.listen(3000, function () {
