e320f197a4f5d7dbd9cbce8b3e3156662f2d85df
Alana Barrett-Frew <alanabarrett-frew@hotmail.com>
2023-05-05 12:15:38 +0100

    code prepared for deployment

--- app.js
+++ app.js
@@ -61,8 +61,8 @@ app.post("/failure", function(req, res) {
 });
 
 
-app.listen(3000, function () {
-  console.log("Server is running on port 3000.");
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server is running on 3000");
 });
 
 //APIkey
