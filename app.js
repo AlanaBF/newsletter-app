2fd73c4ed2e8ad0f169c3042e187b1867f11096e
Alana Barrett-Frew <alanabarrett-frew@hotmail.com>
2023-05-05 11:32:19 +0100

    application connected to mailchimp and working

--- app.js
+++ app.js
@@ -1,10 +1,72 @@
 const express = require("express");
-const request = require("request");
 const bodyParser = require("body-parser");
-
+const request = require("request");
+const https = require("https");
 const app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  let data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+  let jsonData = JSON.stringify(data);
+
+  const url = "https://us21.api.mailchimp.com/3.0/lists/927a37fe5e";
+
+  const options = {
+    method: "POST",
+    auth: "alana:47f147d229f746bc142eb8da4d7ed45d-us21",
+  };
+
+  const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+    response.on("data", function (data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+});
+
+
+
+app.post("/failure", function(req, res) {
+  res.redirect("/");
+});
 
 
 app.listen(3000, function () {
   console.log("Server is running on port 3000.");
 });
+
+//APIkey
+// "47f147d229f746bc142eb8da4d7ed45d-us21"
+
+//list id
+//927a37fe5e
