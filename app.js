5ee0fcb55c93934f95c5917c5df8cc118045a12a
Alana Barrett-Frew <alanabarrett-frew@hotmail.com>
2023-05-07 15:06:11 +0100

    alternative port added

--- app.js
+++ app.js
@@ -68,6 +68,6 @@ app.post("/failure", function(req, res) {
 });
 
 //specify to listen on localhost:3000
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server is running on 3000");
 });
